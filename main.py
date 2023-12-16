0ed10356a75bef5cd40bf4249bdd48e260a25bc1
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-16 12:05:08 +0200

    basic setup

---
+++ main.py
@@ -0,0 +1,21 @@
+import pika
+
+# Подключение к серверу RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+# Создание очереди (если её нет)
+channel.queue_declare(queue='hello')
+
+
+# Функция, которая будет вызываться при получении сообщения
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+
+# Указание функции обратного вызова для обработки сообщений
+channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+# Запуск бесконечного цикла ожидания сообщений
+channel.start_consuming()
