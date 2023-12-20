929d01a2f4ac1b1db2e7565c0e75e259e8dd5950
Grigory Malanchuk <grishamalanchuk@gmail.com>
2023-12-20 23:35:31 +0200

    updated the content of the message

--- src/config.py
+++ src/config.py
@@ -8,5 +8,7 @@ RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
 RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
 RABBITMQ_PASS = os.environ.get('RABBITMQ_PASS')
 LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 logger.add("email.log", rotation="1 week", format="{level} | {time} | {message}", level=LOGGING_LEVEL)
